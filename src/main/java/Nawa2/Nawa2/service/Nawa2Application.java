3717b88f0110a270f4653d8857ae674bd43db9fa
majayong71 <com4032647@gmail.com>
2022-07-08 21:11:02 +0900

    MVC구조화 중

---
+++ src/main/java/Nawa2/Nawa2/service/Nawa2Application.java
@@ -0,0 +1,13 @@
+package Nawa2.Nawa2.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nawa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nawa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Nawa2/Nawa2/Nawa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Nawa2.Nawa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nawa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
